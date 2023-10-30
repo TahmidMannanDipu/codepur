28ccfd71f79383aded6e63a865b1ab9aea9046a7
pialridealike <abdullah@ridealike.com>
2023-10-30 17:38:39 +0600

    day 5

---
+++ lib/homepage.dart
@@ -0,0 +1,17 @@
+// homepage.dart
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Text(
+          'Flutter Developer',
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+    );
+  }
+}
